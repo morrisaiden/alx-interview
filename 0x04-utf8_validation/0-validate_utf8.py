3406956e47d5c6def9927f874df02d48718e7de4
morrisaiden <mwaimorris1997@gmail.com>
2024-08-23 11:45:41 +0300

    UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,38 @@
+def validUTF8(data):
+    num_bytes = 0
+    mask1 = 1 << 7
+    mask2 = 1 << 6
+
+    for byte in data:
+        mask = 1 << 7
+        if num_bytes == 0:
+
+            while byte & mask:
+                num_bytes += 1
+                mask >>= 1
+
+            if num_bytes == 0:
+                continue
+
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+
+            if not (byte & mask1 and not (byte & mask2)):
+                return False
+
+        num_bytes -= 1
+
+    return num_bytes == 0
+
+
+if __name__ == "__main__":
+    data = [65]
+    print(validUTF8(data))
+
+    data = [80, 121, 116, 104, 111, 110,
+            32, 105, 115, 32, 99, 111, 111, 108, 33]
+    print(validUTF8(data))
+
+    data = [229, 65, 127, 256]
+    print(validUTF8(data))
