9dcda26237c2275c5b88db509649403244854d8f
morrisaiden <mwaimorris1997@gmail.com>
2024-09-25 12:15:31 +0300

    Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in the grid."""
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                # Add 4 for the land cell
+                perimeter += 4
+
+                if j < cols - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 2
+
+                if i < rows - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 2
+
+    return perimeter
